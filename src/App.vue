60a4833d213ca96a68e9105cbf1e24dc04496829
Sboonny <MuhammedElruby@gmail.com>
2022-06-01 21:28:01 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section id="drum-machine">
+    <div id="display">
+      <button class="drum-pad"><audio class="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+          id="Q">Q</audio></button>
+      <button class="drum-pad"><audio class="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+          id="W">W</audio></button>
+      <button class="drum-pad"><audio class="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+          id="E">E</audio></button>
+      <button class="drum-pad"><audio class="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+          id="A">A</audio></button>
+      <button class="drum-pad"><audio class="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+          id="S">S</audio></button>
+      <button class="drum-pad"><audio class="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+          id="D">D</audio></button>
+      <button class="drum-pad"><audio class="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+          id="Z">Z</audio></button>
+      <button class="drum-pad"><audio class="clip" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+          id="X">X</audio></button>
+      <button class="drum-pad"><audio class="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+          id="C">C</audio></button>
+    </div>
+  </section>
+</template>
+
+<style>
+</style>
